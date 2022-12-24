82552cf10b9c2c231eed25a71c430b0833cb5c1d
João Salinas <joao.salinas@outlook.com>
2022-12-24 11:13:52 -0300

    Remove Context de Calc

--- src/components/FormCalculator/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import FormCompoundInterestCalculator from '.'
-
-export default {
-  title: 'FormCompoundInterestCalculator',
-  component: FormCompoundInterestCalculator
-} as Meta
-
-export const Default: Story = () => <FormCompoundInterestCalculator />
